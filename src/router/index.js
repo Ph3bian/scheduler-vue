37e328fab23e63a2bf4dab2934e5323ca6714c7f
phebian <phebian.chukwurah@chlela.com>
2021-01-18 14:08:23 +0100

    router bug still not gone

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,23 @@
-import { createRouter } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
   {
     path: "/sites",
     name: "Sites",
-    component: () =>
-      import(/* webpackChunkName: "sites" */ "@/views/Sites.vue"),
+    component: () => import(/* webpackChunkName: "sites" */ "@/views/Sites.vue")
   },
   {
     path: "/sites/:id",
     name: "Site",
     props: true,
-    component: () => import(/* webpackChunkName: "site" */ "@/views/Site.vue"),
+    component: () => import(/* webpackChunkName: "site" */ "@/views/Site.vue")
   },
-  { path: "/", redirect: "/sites" },
+  { path: "/", redirect: "/sites" }
 ];
 
-const router = createRouter({ routes });
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+});
 
 export default router;
