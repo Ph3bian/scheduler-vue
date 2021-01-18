24a779f163f0e03070c76d18e7fe6ac62b432f85
phebian <phebian.chukwurah@chlela.com>
2021-01-18 10:19:14 +0100

    go back

--- src/views/Site.vue
+++ src/views/Site.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="Site">
     <div class="SiteHeader">
-      <div>
+      <div @click="$router.push('/sites')">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
           <path d="M0 0h24v24H0z" fill="none" />
           <path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z" />
